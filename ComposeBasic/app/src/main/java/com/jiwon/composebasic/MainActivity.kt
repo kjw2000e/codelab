9b979a9135454b25f9f0edf71d0aa15dca7c909f
jiwon <kjw2000e@2beone.net>
2024-02-29 17:44:49 +0900

    [ComposeBasic] 3. state in compose

--- ComposeBasic/app/src/main/java/com/jiwon/composebasic/MainActivity.kt
+++ ComposeBasic/app/src/main/java/com/jiwon/composebasic/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +45,15 @@ fun MyApp(modifier: Modifier = Modifier,
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // 상태값이 변경이 되었을때 해당 부분만 변경한다.
+    // 컴포즈에서 상태값 추적을 하기 위해서는 MutableStateOf 함수를 사용해야한다.
+    // 재구성이 여러번 발생할 수 있어 단독으로 할당하진 못하고 remeber키워드가 필요.
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val extrapadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -54,14 +65,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         // weight 수정자는 사용 가능한 공간 전체를 채운다. 가중치가 없는 다른 요소를 효과적으로 밀어냄.
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extrapadding)
             ) {
                 Text( text = "Hello" )
                 Text( text = "$name!")
             }
 
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text("Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
