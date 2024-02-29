6e07437d9154972f2dbf5cef114a757b9d2cdafd
jiwon <kjw2000e@2beone.net>
2024-02-29 17:21:42 +0900

    [ComposeBasic] 3. columns and rows, weight

--- ComposeBasic/app/src/main/java/com/jiwon/composebasic/MainActivity.kt
+++ ComposeBasic/app/src/main/java/com/jiwon/composebasic/MainActivity.kt
@@ -3,8 +3,12 @@ package com.jiwon.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,28 +30,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Android")
+fun MyApp(modifier: Modifier = Modifier,
+          names: List<String> = listOf("word", "compose")) {
+
+    // Column 중첩 ,
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
-        color = MaterialTheme.colorScheme.primary
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+        // row : horizontal 배치
+        // Column : vertical 배치
+
+        // 행 끝으로 배치하는 방법 : weight 가중치 만들기
+        // weight 수정자는 사용 가능한 공간 전체를 채운다. 가중치가 없는 다른 요소를 효과적으로 밀어냄.
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text( text = "Hello" )
+                Text( text = "$name!")
+            }
+
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true, name = "미리보기")
+@Preview(showBackground = true, name = "미리보기", widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeBasicTheme {
