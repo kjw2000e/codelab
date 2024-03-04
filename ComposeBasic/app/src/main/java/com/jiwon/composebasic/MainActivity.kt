c015987f453cda88db58d7e2dfe3352adcc908f1
jiwon <kjw2000e@2beone.net>
2024-03-04 10:17:10 +0900

    [ComposeBasic] 9.Finishing touches!
    아이콘 변경, Card 적용

--- ComposeBasic/app/src/main/java/com/jiwon/composebasic/MainActivity.kt
+++ ComposeBasic/app/src/main/java/com/jiwon/composebasic/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -14,8 +15,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,10 +56,15 @@ class MainActivity : ComponentActivity() {
 fun MyApp(modifier: Modifier = Modifier,
           names: List<String> = listOf("word", "compose")) {
 
-    // Column 중첩 ,
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name)
+    var shouldShowOnBoarding by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
+        if (shouldShowOnBoarding) {
+            OnboardingScreenByHoisting(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            Greetings()
         }
     }
 }
@@ -104,9 +118,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             }
 
             ElevatedButton(onClick = { expanded = !expanded }) {
-                Text(if (expanded) "Show less" else "Show more")
+                Text(if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                })
             }
         }
+    }
+}
+
+@Composable
+fun GreetingCard(name: String, modifier: Modifier = Modifier) {
+    Card(
+       colors = CardDefaults.cardColors(
+           containerColor = MaterialTheme.colorScheme.primary
+       ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
     }
 }
 
@@ -126,7 +156,55 @@ private fun Greetings(
 //        }
 
         items(items = names) { name ->
-            Greeting(name = name)
+//            Greeting(name = name)
+            GreetingCard(name = name)
+        }
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                    "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded} ) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
@@ -197,8 +275,8 @@ fun MyAppHoisting(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeBasicTheme {
-        Greetings()
-//        MyApp()
+//        Greetings()
+        MyApp()
 //        OnboardingScreen()
     }
 }
